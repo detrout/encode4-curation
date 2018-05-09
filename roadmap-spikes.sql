18250ace58c164fcd884a63dfbc6962b11c9bdb9
Diane Trout <diane@ghic.org>
2018-05-09 15:15:06 -0700

    What spikeins are roadmail files annotated with?
    (Answer: None, but their protocol docs say they used spikes...)

---
+++ roadmap-spikes.sql
@@ -0,0 +1,81 @@
+\pset format unaligned
+\pset fieldsep '\t'
+with
+  experiment as (
+  select uri as Experiment,
+         payload->>'accession' as Experiment_Accession,
+         payload->>'description' as Experiment_Description,
+         payload->>'status' as Experiment_Status,
+         payload->>'date_released' as Experiment_Released,
+         payload->>'assay_title' as Experiment_term_name,
+         payload->>'assay_term_id' as Experiment_term_id,
+         payload->>'award' as Award
+  from item
+  where object_type = 'Experiment'
+  ),
+  file as (
+  select uri as File,
+         payload->>'accession' as File_Accession,
+         payload->>'dataset' as dataset,
+         payload->>'status' as File_Status,
+         payload->>'assembly' as assembly,
+         payload->>'file_format' as file_format,
+         payload->>'output_type' as output_type,
+         payload->>'submitted_file_name' as filename,
+         payload->>'biological_replicates' as biorep,
+         payload->>'technical_replicates' as techrep,
+         payload->>'notes' as notes
+  from item
+  where object_type= 'File' and
+        payload->'lab' @> '"/labs/barbara-wold/"'::jsonb
+  ),
+  replicate as (
+    select uri as Replicate,
+           payload->>'experiment' as Experiment,
+           payload->>'library' as Library
+    from item
+    where object_type = 'Replicate'
+  ),
+  library as (
+    select uri as Library,
+           payload->>'accession' as Library_Accession,
+           payload->>'date_created' as Library_Created,
+           payload->>'biosample' as Biosample,
+           payload->>'spikeins_used' as spikeins,
+           substring(jsonb_array_elements_text(payload->'aliases') from 'barbara-wold:([0-9_A-Z]+)') as library_id
+    from item
+    where object_type = 'Library'
+  ),
+  biosample as (
+     select uri as Biosample,
+            payload->>'accession' as Biosample_Accession,
+            payload->>'organism' as Organism,
+            payload->>'summary' as Summary,
+            payload->>'award' as award,
+            payload->>'biosample_term_id' as biosample_term_id,
+            payload->>'biosample_term_name' as biosample_term_name
+        from item
+        where object_type = 'Biosample'
+  ),
+  award as (
+     select uri as Award,
+            payload->>'rfa' as rfa
+     from item
+     where object_type = 'Award'
+  )
+select distinct
+  award.rfa,
+  experiment.Experiment,
+  experiment.Experiment_term_name,
+  Library_Accession,
+  spikeins,
+  experiment.Experiment_Description
+
+from experiment
+     left join award on experiment.Award = award.Award
+     left join replicate on replicate.Experiment = experiment.Experiment
+     left join library on replicate.Library = library.Library
+where rfa = 'Roadmap'
+order by experiment_term_name
+
+;
